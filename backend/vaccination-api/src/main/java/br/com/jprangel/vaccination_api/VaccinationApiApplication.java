cc4dd8f5d97de2500ccd7a193234a9ec9934c04c
jpmrangel <joaopaulomr96@gmail.com>
2025-10-25 15:26:27 -0300

    chore(backend): initial spring boot project setup

---
+++ backend/vaccination-api/src/main/java/br/com/jprangel/vaccination_api/VaccinationApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.jprangel.vaccination_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccinationApiApplication.class, args);
+	}
+
+}
---
+++ backend/vaccination-api/src/test/java/br/com/jprangel/vaccination_api/VaccinationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jprangel.vaccination_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
